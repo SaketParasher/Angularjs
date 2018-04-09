155abe051880e9a0515c0eac7b79090b9c9e1359
SaketParasher <saket.parasher@gmail.com>
2018-04-09 20:51:15 +0530

    session complete ofc

--- app/js/controllers/EventDetailsController.js
+++ app/js/controllers/EventDetailsController.js
@@ -8,4 +8,12 @@ eventsApp.controller('EventDetailsController',function($scope,eventDataService,$
     $scope.reload = function(){
         $route.reload();
     }
+    
+    $scope.upVotecnt = function(sess){
+        sess.upVoteCount++;
+    }
+    $scope.downVotecnt = function(sess){
+        if(sess.upVoteCount>0)
+        sess.upVoteCount--;
+    }
 });
